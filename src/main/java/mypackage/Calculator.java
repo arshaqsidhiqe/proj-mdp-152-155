5bd8a6def04298a2a05141e60ec6b80c440a56b6
arshaqsidhiqe <arshaqsidhiqe@gmail.com>
2023-01-31 09:06:25 +0530

    Added back multi fun both main & test code

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -16,7 +16,10 @@ public class Calculator extends HttpServlet
 		return second-first;
 	}
 	
-
+    public long mulFucn(long first, long second){
+		
+		return first*second;
+	}
 	
 	
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -27,9 +30,6 @@ public class Calculator extends HttpServlet
         PrintWriter out= response.getWriter();
         int a1= Integer.parseInt(request.getParameter("n1"));
         int a2= Integer.parseInt(request.getParameter("n2"));
-        
-        
-        
         if(request.getParameter("r1")!=null)
         {
             out.println("<h1>Addition</h1>"+addFucn(a1, a2));
@@ -37,6 +37,10 @@ public class Calculator extends HttpServlet
         if(request.getParameter("r2")!=null)
         {
             out.println("<h1>Substraction</h1>"+subFucn(a1, a2));
+        }
+        if(request.getParameter("r3")!=null)
+        {
+            out.println("<h1>Multiplication</h1>"+mulFucn(a1, a2));
         }
        
         RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");  
